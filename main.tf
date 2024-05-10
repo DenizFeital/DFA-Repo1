bc1683f252e19a8e9e32d3de944061cc1933f276
Deniz Feital Armanhe <49575782+DenizFeital@users.noreply.github.com>
2024-05-10 16:13:21 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,8 +6,18 @@ terraform {
         }
     }
 backend "azurerm" {
-    resource_group_name = "DFA-RG-ABC"
+    resource_group_name = "cloud-shell-storage-eastus"
     storage_account_name = "dfatf"
     container_name = "dfacontainertf"
     key = "abc.dfakeyfile"
+}
+}
+
+provider "azurerm" {
+  features {}
+}
+#Create Resource Group!!
+resource "azurerm_resource_group" "DFA-RG-ABC" {
+  name = "DFA-RG-ABCTF"
+  location = "eastus"
 }
